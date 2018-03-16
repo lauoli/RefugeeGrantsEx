b22c70d05763709e4b92638bfeef8911ea189469
Laura Olivieri <lauraolivieri@Lauras-MacBook-Pro-2.local>
2018-03-16 14:42:17 -0400

    listed variables

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready()
+var NuFamMembers,
+	var GrantPP2018 = 1000,
+		var SpeakEnglish,
+			var TotalFamGrant;
+
+// Since we have the expenses per person, we could also calculate how much pocket money the families will have so they can get a better idea of their financial situation and be able to manage it better.
+
+var singleMonthlyExpense = 800,
+	var coupleMonthlyExpenses = 950,
+		var thresomeMonthlyExpense = 1000,
+			var foursomeMonthylyExpense = 1200;
